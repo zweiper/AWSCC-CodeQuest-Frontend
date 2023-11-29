8ec486ffba5d46e80430ce362da8101e53c532ce
zweiper <jessicaerasmo21@gmail.com>
2023-11-30 00:24:38 +0800

    html structure revision, script template,  and style for works page
    adjusted the html structure of the works page and applied the needed css styles for it. I also copied a script template from w3school for my desired effect. Although it's not working as of now.

---
+++ projects/02-your-own-portoflio/script.js
@@ -0,0 +1,21 @@
+// Source: https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_imgdots
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+  }
+  
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("img-section");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+    }
+    x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " w3-opacity-off";
+  }
