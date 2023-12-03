ee9b6133865fea61c0b436808153a09b037455d2
zweiper <jessicaerasmo21@gmail.com>
2023-12-03 21:25:19 +0800

    Task 2 of Day 13 JavaScript
    Successfully initialized and defined the variables

--- frontend-lessons/javascript-tasks/day-13/day-13-tasks/day13.js
+++ frontend-lessons/javascript-tasks/day-13/day-13-tasks/day13.js
@@ -1,3 +1,12 @@
+// Task 1: Variable Mastery
 let currentLesson = 'JavaScript Fundamentals';
 
 console.log(currentLesson);
+
+// Task 2: Data Types Exploration
+
+let favoriteNumber = 21;
+let favoriteWord = 'wao!';
+let isJavaScriptFun = true;
+let nothing = null;
+let undefinedVar;
