a7b7610e83f4a127fbf1ba8eecd0fb8211bf4866
zweiper <jessicaerasmo21@gmail.com>
2023-12-03 21:41:37 +0800

    Task 3 of Day 13 JavaScript
    Successfully used logical operators to compare values

--- frontend-lessons/javascript-tasks/day-13/day-13-tasks/day13.js
+++ frontend-lessons/javascript-tasks/day-13/day-13-tasks/day13.js
@@ -4,9 +4,20 @@ let currentLesson = 'JavaScript Fundamentals';
 console.log(currentLesson);
 
 // Task 2: Data Types Exploration
-
 let favoriteNumber = 21;
 let favoriteWord = 'wao!';
 let isJavaScriptFun = true;
 let nothing = null;
 let undefinedVar;
+
+// Task 3: Logical Operators in Action
+let isMorning = false;
+let isRaining = false;
+
+if(isMorning != false && isRaining != false) {
+    console.log("It is raining in the morning! D:");
+} else if(isMorning == true || isRaining == true) {
+    console.log("It is either morning or raining! :O");
+} else {
+    console.log("It is neither morning nor raining :)");
+}
