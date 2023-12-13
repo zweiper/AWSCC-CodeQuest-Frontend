979df11c61fdf83384b79e41825bf56fadf3b5e7
zweiper <jessicaerasmo21@gmail.com>
2023-12-13 18:14:49 +0800

    Day 15 JS Task

--- frontend-lessons/javascript-tasks/day-15/index.js
+++ frontend-lessons/javascript-tasks/day-15/index.js
@@ -1,7 +1,20 @@
 // JavaScript Fundamentals - Day 15: Assignment
 
 // 1. Use a `for` loop to count from 1 to 5 and print each number to the console.
+for(let i = 1; i <= 5; i++) {
+    console.log("Number " + i);
+}
 // 2. Create an array named `cookies` with different types of cookies (strings).
+const cookies = ["chocolate", "raisin", "peanut", "gingerbread"];
 // 3. Use a `for` loop to iterate over the `cookies` array and print each type of cookie to the console.
+console.log("Different types of cookies:");
+for(let i = 0; i < cookies.length; i++) {
+    console.log(cookies[i]);
+}
 // 4.  Create a variable named `jarNotEmpty` and set it to `true`.
+let jarNotEmpty = true;
 // 5.  Use a `while` loop to simulate eating cookies while the jar is not empty.
+while(jarNotEmpty){
+    console.log("Eating cookies!");
+    jarNotEmpty =
+}
